21d0806932dbef8241144023c7d49a4df99320f3
20P31A0497 <84895813+20P31A0497@users.noreply.github.com>
2023-02-22 21:33:23 +0530

    Add files via upload

---
+++ file.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+class file {
+    public static void main(String[] args) {
+        GetFileData objectGetFileData = new GetFileData();
+
+        objectGetFileData.getCompeteDataINArray("billingList.csv", 5);
+
+        int lastID = objectGetFileData.getLastID("billingList", 3);
+        System.out.println(lastID);
+    }
+}
+
+class GetFileData {
+    public String[][] getCompeteDataINArray(String fileName, int countData) {
+        String[][] completeFileDataArray = null;
+
+        try {
+            File fileReader = new File(fileName);
+            Scanner scFileReader = new Scanner(fileReader);
+
+            ArrayList<String> completeFileData = new ArrayList<String>();
+
+            while (scFileReader.hasNext()) {
+                String eachLine = scFileReader.nextLine();
+                completeFileData.add(eachLine);
+            }
+            int sizeCompleteFileData = completeFileData.size();
+            completeFileDataArray = new String[sizeCompleteFileData][countData];
+
+            for (int i = 0; i < sizeCompleteFileData; i++) {
+                String dataLineOne = completeFileData.get(i);
+                for (int j = 0; j < countData; j++) {
+                    if (j == countData - 1) {
+                        completeFileDataArray[i][j] = dataLineOne;
+                    } else {
+                        completeFileDataArray[i][j] = dataLineOne.substring(0, dataLineOne.indexOf(","));
+                        dataLineOne = dataLineOne.substring(dataLineOne.indexOf(",") + 1);
+                    }
+                }
+            }
+
+            // System.out.println(completeFileDataArray[10][2]);
+
+            scFileReader.close();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return completeFileDataArray;
+    }
+
+    public int getLastID(String fileName, int countData) {
+        int lastID = 0;
+        String[][] outputData = this.getCompeteDataINArray(fileName, countData);
+        lastID = Integer.valueOf(outputData[outputData.length - 1][0]);
+        return lastID;
+        }
+
+}
